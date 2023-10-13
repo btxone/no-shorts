5b00b836f3d91635f86828e8cd023f14fae54343
btxone <97922730+btxone@users.noreply.github.com>
2023-10-13 16:42:41 -0300

    multiple links

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url && tab.url.includes("https://www.youtube.com/shorts")) {
-    chrome.tabs.remove(tabId);
+  if (changeInfo.status === "complete" && tab.url) {
+    const blockedSites = ["https://www.youtube.com/shorts", "link de ejemplo", "link de ejemplo"];
+    if (blockedSites.some(site => tab.url.includes(site))) {
+      chrome.tabs.remove(tabId);
+    }
   }
 });
 
